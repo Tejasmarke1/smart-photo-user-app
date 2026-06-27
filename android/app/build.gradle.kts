ddd2d81b223d8a53f6dd82bc454e1e7e4ed915fc
TejasMarke1 <marketejas4@gmail.com>
2026-06-27 13:57:35 +0530

    Enable core library desugaring for Java 8 compatibility inside android app build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,11 +9,7 @@ plugins {
 dependencies {
   // Import the Firebase BoM
   implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
-
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  // https://firebase.google.com/docs/android/setup#available-libraries
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 android {
@@ -24,6 +20,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
